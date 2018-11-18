48cce959dcecf77ab84a9a41ce159926e0fde8b8
Curtis Kesner <ck0562217@otc.edu>
2018-11-18 17:37:28 -0600

    Now have index and about pages working from web server

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
  
-app.get('/', function (req, res) {
-  // res.send('Hello World');
+app.get('/index.html', function (req, res) {
     res.sendFile(path.join(__dirname + "/index.html"));
 });
+app.get('/about.html', function (req, res) {
+    res.sendFile(path.join(__dirname + "/about.html"));
+});
  
+console.log();
+
 app.listen(3000);
